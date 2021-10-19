f43c2b5ddfa11b9fa1040101da2ec58664826178
Francisco Cabrera <fcabrera@microsoft.com>
2021-10-19 14:08:47 -0400

    Update EflowUtil-GetFirewallRules.ps1

--- eflow-util/EflowUtil-GetFirewallRules.ps1
+++ eflow-util/EflowUtil-GetFirewallRules.ps1
@@ -1,8 +1,43 @@
+ <#
+.DESCRIPTION
+    Iptables is used to set up, maintain, and inspect the tables of IP packet filter rules in the Linux kernel.
+
+.PARAMETER table
+    Name of table - Each table contains a number of built-in chains and may also contain user-defined chains.
+
+.PARAMETER chain
+    Name of chain - Each chain is a list of rules which can match a set of packets. 
+
+#>
+
+param (
+
+    [ValidateSet("INPUT", "OUTPUT", "FORWARD", "DOCKER", "DOCKER-ISOLATION-STAGE-1", "DOCKER-ISOLATION-STAGE-2", "DOCKER-USER")]
+    [String] $chain,
+
+    [ValidateSet("filter", "nat", "mangle", "raw")]
+    [String] $table
+)
+
+
 try
 {
     Import-Module AzureEflow
 
-    $result = Invoke-EflowVmCommand -command 'sudo iptables -L' -ignoreError
+    [String]$vmCommand = "sudo iptables -L "
+
+    if (![string]::IsNullOrEmpty($table))
+    {
+        $vmCommand += " --table $($table)"  
+    }
+
+     if (![string]::IsNullOrEmpty($chain))
+    {
+        $vmCommand += " $($chain)"  
+    }
+
+
+    $result = Invoke-EflowVmCommand -command $vmCommand -ignoreError
     
     if([string]::IsNullOrEmpty($result))
     {
